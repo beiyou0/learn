d086233248f3b77abd734dda05e58e2ffb3ba684
Qian Qian Liu <liuqqbj@cn.ibm.com>
2018-01-09 11:30:33 +0800

    add bubblesort

--- src/main/java/org/learn/algorithm/Sorting.java
+++ src/main/java/org/learn/algorithm/Sorting.java
@@ -157,7 +157,15 @@ public class Sorting {
     }
 
     public static void bubbleSort(int[] a) {
-
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 1; j < a.length - i; j++) {
+                if (a[j-1] > a[j]) {
+                    int temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
+                }
+            }
+        }
     }
 
 
@@ -168,7 +176,8 @@ public class Sorting {
 //        mergeSort(a, 0, a.length -1);
 //        quickSort(a, 0, a.length - 1);
 //        randomQuickSort(a, 0, a.length -1);
-        heapSort(a);
+//        heapSort(a);
+        bubbleSort(a);
 
         for(int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
